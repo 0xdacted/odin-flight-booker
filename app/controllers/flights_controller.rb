de44b7a99432532c6f524eb7c66ff3f492e9d10a
Red <crabmarket56@gmail.com>
2022-10-04 16:15:20 +0900

    Fix misc bugs

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,8 @@ class FlightsController < ApplicationController
 
   def index
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id]}
-    @year_options = Flight.all.map { |flight| [flight.departure_time.year].uniq }
-    @month_options = Flight.all.map { |flight| [flight.departure_time.month].uniq }
-    @month_options = Flight.all.map { |flight| [flight.departure_time.day].uniq }
+    @year_options = Flight.all.map { |flight| [flight.departure_time.year] }.uniq
+    @month_options = Flight.all.map { |flight| [flight.departure_time.month] }.uniq
+    @day_options = Flight.all.map { |flight| [flight.departure_time.day] }.uniq
   end
 end
