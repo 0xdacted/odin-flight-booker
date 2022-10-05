da09aa5d266ae47b01a43f4e5cce9432a863ff75
Red <crabmarket56@gmail.com>
2022-10-05 15:42:03 +0900

    Correct search form/results logic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,18 +2,14 @@ class FlightsController < ApplicationController
 
   def index
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id]}
-    @year_options = Flight.all.map { |flight| [flight.departure_time.year] }.uniq
-    @month_options = Flight.all.map { |flight| [flight.departure_time.month] }.uniq
-    @day_options = Flight.all.map { |flight| [flight.departure_time.day] }.uniq
+    @date_options = Flight.all.map { |flight| [flight.departure_time] }.uniq
     @available_flights = find_flights
   end
 
   def find_flights
     Flight.where(departure_airport: params[:departure_airport],
                 arrival_airport: params[:arrival_airport],
-                year: params[:year],
-                month: params[:month],
-                day: params[:day]
+                departure_time: params[:departure_time]
     )
   end
 
