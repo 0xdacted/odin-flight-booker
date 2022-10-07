3151a300cb661ecd9818d109369a9dd6a57fe176
Red <crabmarket56@gmail.com>
2022-10-07 09:07:15 +0900

    Create passenger model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_070334) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_000549) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,6 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_070334) do
     t.integer "arrival_id"
     t.datetime "departure_time"
     t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
