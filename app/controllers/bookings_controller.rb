c496bc5032dcd6f413f9049f579f0f0b8f0b522a
Red <crabmarket56@gmail.com>
2022-10-07 14:22:50 +0900

    Draft booking logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
 
   def new
-
+    @booking = Booking.new
+    @selected_flight = Flight.find(params[:booking_option])
+    @pax = params[:passengers].to_i
+    @pax.times {@booking.passengers.build }
   end
 end
